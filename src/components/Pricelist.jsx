caeaa07733b89043480dfe75bccbeaaa462b879b
rivldy <akbar22rivaldy@gmail.com>
2022-04-14 07:44:55 +0700

    Fix grid responsiveness

--- src/components/Pricelist.jsx
+++ src/components/Pricelist.jsx
@@ -44,9 +44,9 @@ const cards = [
 export default function Pricelist() {
     return (
         <div className="price-list">
-            <h1 className="text-5xl font-bold text-center">Get more than just a VPN</h1>
+            <h1 className="text-4xl sm:text-5xl font-bold text-center">Get more than just a VPN</h1>
             <p className="text-lg text-center pt-6">Block malware, trackers, and intrusive ads with FREE Threat Protection.</p>
-            <div className="grid grid-cols-1 sm:grid-cols-3 my-10 gap-2">
+            <div className="grid grid-cols-1 lg:grid-cols-3 my-10 gap-2">
                 { cards.map((card, id) => (
                     <PriceCard card={card} key={id} />
                 )) }
