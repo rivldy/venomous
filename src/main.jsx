8fe192a4f83f7a7327edd0aebf14371b434eb7bc
Akbar Rivaldy <akbar22rivaldy@gmail.com>
2022-04-17 07:22:40 +0700

    Dynamic title depending on route
    Add new feature:
    Page title will change according to the active route using react-helmet-async

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom'
 import store from './app/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import "./styles/index.css"
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
