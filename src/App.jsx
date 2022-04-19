fc172e81dc6fc9749516a16e9b03c3906567642b
Akbar Rivaldy <akbar22rivaldy@gmail.com>
2022-04-19 11:42:50 +0700

    Remove blog route & add blog, login routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import RouteChange from './utils/RouteChange'
 import Home from './routes/Home'
 import Pricing from './routes/Pricing'
-import Help from './routes/Help'
+import Blog from './routes/Blog'
 import Contact from './routes/Contact'
+import Login from './routes/Login'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -15,8 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pricing" element={<Pricing />} />
-          <Route path="/help" element={<Help />} />
+          <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </RouteChange>
       <Footer />
